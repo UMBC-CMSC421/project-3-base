61fb97c2b3c05dd94efddbbba5921dbb2838282c
aangar <zekrom514@gmail.com>
2025-10-13 08:57:27 -0400

    change - final code

--- proj3KernelTemplate.h
+++ proj3KernelTemplate.h
@@ -15,9 +15,10 @@
 
 // the Queue structure
 typedef struct priority_queue_421 {
-  queue_node_421_t* head;
-  queue_node_421_t* tail;
-  int num_nodes;
+  queue_node_421_t*   head;
+  queue_node_421_t*   tail;
+  struct mutex*       lock;
+  int                 num_nodes;
 } priority_queue_421_t;
 
 // the application context which stores each queue
